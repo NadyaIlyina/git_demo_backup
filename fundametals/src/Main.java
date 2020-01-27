a4484106bba47a2aa869eab84841c3706a047656
Nadya Ilina <nadya_ilina_1998@mail.ru>
2020-01-27 09:37:12 +0300

    add fundametails

---
+++ fundametals/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*MainTask mainTask = new MainTask();
+        mainTask.tasks(args);*/
+
+        /*OptionalTask2 optionalTask1 = new OptionalTask2();
+        optionalTask1.start();*/
+    }
+}
